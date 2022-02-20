581bc21786897651ee82ed97535c9dbf706bbb8e
Yana <yanatanasova24@gmail.com>
2022-02-20 21:50:52 +0200

    attempts to put data into cells

--- custom.js
+++ custom.js
@@ -6,10 +6,19 @@ class Table{
 		this._bodyData = bodyData;
 	}
 
-
-
-	static buildTable(numR, numC){
+	buildTable(){
 		let table, row, cell;
+
+		let numC = this._headerData.length; 
+		let numR = this._bodyData.length + 1;
+		
+		let headerTitle = this._headerData.map(function(item){
+			return item.title;
+		});
+
+		let dataValues = this._bodyData.map(element => {
+			return Object.values(element);
+		});
 
 		table = document.createElement('table');
 		table.style.borderCollapse = 'collapse';
@@ -21,7 +30,14 @@ class Table{
 			for (let j=1; j<=numC; j++) {
 
 				cell = document.createElement(i === 1 ? 'th' :'td');
-				cell.appendChild(document.createTextNode('gjgbgvj'));
+
+				if(i === 1){
+
+					cell.appendChild(document.createTextNode(headerTitle[j - 1]));
+				
+				} else {
+					cell.appendChild(document.createTextNode(dataValues[i - 2][j]));
+				}
 
 				cell.style.padding = '10px';
 				cell.style.border = '1px solid #000';
@@ -35,4 +51,95 @@ class Table{
 
 }
 
-Table.buildTable(5, 10);
+
+
+const headerConfig = [
+	{
+		id: 'title',
+		title: 'Title',
+	},
+	{
+		id: 'description',
+		title: 'Description',
+	},
+	{
+		id: 'quantity',
+		title: 'Quantity',
+	},
+	{
+		id: 'status',
+		title: 'Status',
+	},
+	{
+		id: 'price',
+		title: 'Price',
+	},
+	{
+		id: 'discount',
+		title: 'Discount',
+	},
+	{
+		id: 'sales',
+		title: 'Sales',
+	}
+];
+
+
+const data = [
+	{
+		'id': 's11',
+		'title': 'Some title',
+		'description': 'la-la-la',
+		'quantity': 20,
+		'status': 1,
+		'price': 3,
+		'discount': 0,
+		'sales': 14
+	},
+	{
+		'id': 's10',
+		'title': 'Some title 2',
+		'description': 'la-la-la',
+		'quantity': 5,
+		'status': 0,
+		'price': 34,
+		'discount': 0,
+		'sales': 9
+	},
+	{
+		'id': 's12',
+		'title': 'Some title 3',
+		'description': 'la-la-la',
+		'quantity': 7,
+		'status': 0,
+		'price': 567,
+		'discount': 0,
+		'sales': 1
+	},
+	{
+		'id': 's13',
+		'title': 'Some title 4',
+		'description': 'la-la-la',
+		'quantity': 15,
+		'status': 0,
+		'price': 34,
+		'discount': 0,
+		'sales': 17
+	},
+	{
+		'id': 's14',
+		'title': 'Some title 5',
+		'description': 'la-la-la',
+		'quantity': 3,
+		'status': 0,
+		'price': 467,
+		'discount': 0,
+		'sales': 5
+	}
+]
+
+// console.log(Object.keys(data[0]));
+
+let tableExample = new Table(headerConfig, data);
+
+tableExample.buildTable();
